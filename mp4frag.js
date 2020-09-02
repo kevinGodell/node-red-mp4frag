103325d397ea60ddf4bf204d1d454c10ffc5d4ab
Kevin Godell <kevin.godell@gmail.com>
2020-09-01 20:18:03 -0500

    add options to select context location

--- mp4frag.js
+++ mp4frag.js
@@ -7,30 +7,49 @@ Mp4Frag.prototype.toString = function () {
   return `Mp4Frag({ hlsBase: '${this._hlsBase}', hlsListSize: ${this._hlsListSize} })`;
 };
 
-module.exports = function (RED) {
+module.exports = RED => {
+  // sets context and returns a function to unset context
+  function setContext(node, contextAccess, uniqueName, mp4frag) {
+    switch (contextAccess) {
+      case 'global':
+        const globalContext = node.context().global;
+
+        globalContext.set(uniqueName, mp4frag);
+
+        return () => {
+          globalContext.set(uniqueName, undefined);
+        };
+
+      case 'flow':
+        const flowContext = node.context().flow;
+
+        flowContext.set(uniqueName, mp4frag);
+
+        return () => {
+          flowContext.set(uniqueName, undefined);
+        };
+
+      default:
+        return () => {};
+    }
+  }
+
   function Mp4FragNode(config) {
     RED.nodes.createNode(this, config);
 
-    const { hlsBase, hlsListSize } = config;
-
-    const globalContext = this.context().global;
-
-    // check if hlsBase is already used by another node
-    if (globalContext.get(hlsBase) !== undefined) {
-      const err = `hlsBase '${hlsBase}' already in use`;
-
-      this.status({ fill: 'red', shape: 'dot', text: err });
-
-      return this.error(err);
-    }
+    const { uniqueName, hlsListSize, contextAccess, httpRoutes } = config;
 
     try {
       // mp4frag can throw if given bad hlsBase
-      const mp4frag = new Mp4Frag({ hlsBase, hlsListSize });
+      const mp4frag = new Mp4Frag({ hlsBase: uniqueName, hlsListSize });
 
-      globalContext.set(hlsBase, mp4frag);
+      const unsetContext = setContext(this, contextAccess, uniqueName, mp4frag);
 
       const onInitialized = data => {
+        // todo this.send({cmd: 'start', type: 'hls', payload: '/uri_to_playlist.m3u8'});
+        // todo cmd 'start' to trigger front end video playback to start
+        // todo pass uri to playlist.m3u8
+
         this.status({ fill: 'green', shape: 'dot', text: 'initialized' });
       };
 
@@ -48,18 +67,21 @@ module.exports = function (RED) {
         const { payload } = msg;
 
         if (Buffer.isBuffer(payload) === true) {
-          return mp4frag.write(payload);
+          mp4frag.write(payload);
+        } else {
+          // receiving anything else will signal us to clear mp4frag cache
+          // todo maybe handle input msgs
+
+          mp4frag.resetCache();
+
+          this.status({ fill: 'green', shape: 'ring', text: 'ready' });
+
+          // todo this.send({cmd:'stop') to trigger front end video playback to stop
         }
-
-        const err = 'input must be a buffer';
-
-        this.status({ fill: 'red', shape: 'dot', text: err });
-
-        this.error(err);
       };
 
       const onClose = (removed, done) => {
-        globalContext.set(hlsBase, undefined);
+        unsetContext();
 
         mp4frag.resetCache();
 
